4e3618c2252a18c5ac9b14f3d0a36962495c94c6
Crowdbotics <team@crowdbotics.com>
2022-03-07 21:08:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "esp_calculator_34041",
+  url: "https://esp_calculator_34041.botics.co",
+  api: "https://esp_calculator_34041.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
